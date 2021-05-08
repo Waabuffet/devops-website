4630b81450798516351c87d504fa8545f0328bda
Dominique <dominousas@gmail.com>
2021-05-08 14:31:09 +0300

    returning error in case of no sql cnx

--- api/getmessage.php
+++ api/getmessage.php
@@ -16,10 +16,10 @@ $json_resp = null;
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-	$json_resp = ['response' => $row['message']];  
-}
+    $json_resp = ['response' => $row['message']];  
+  }
 } else {
-	$json_resp = ['response' => 'No rows fetched'];
+	$json_resp = ['error' => 'No rows fetched'];
 }
 $conn->close();
 
